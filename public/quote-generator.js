1f6d21c5ae2fcecb9b1019caf545bb3566ca932a
4132thegreat-lang <4132thegreat@gmail.com>
2026-04-19 07:53:35 +0530

    Add Inter font to PDF payload to fix missing glyphs (tofu)

--- public/quote-generator.js
+++ public/quote-generator.js
@@ -1844,6 +1844,7 @@ window.printQuote = async function () {
   <meta charset="UTF-8">
   <title>${userFilename.replace('.pdf', '')}</title>
   <base href="${window.location.origin}/">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="${window.location.origin}/style.css">
   <link rel="stylesheet" href="${window.location.origin}/quote-generator.css">
   <style>
